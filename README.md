44a74048a3dd8ed39f82c2712f6da470cf7326a5
cdemi <christopher.demicoli@outlook.com>
2021-09-24 11:22:59 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# TheBetterSlots.com Website
+
+This is the Jekyll website for [TheBetterSlots.com](https://TheBetterSlots.com). To run this website:
+* Install Ruby 3.0
+* Run `bundle install`
+* Run `bundle exec jekyll serve`
---
+++ src/service-worker.js
@@ -0,0 +1,70 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp(/.*(facebook\.(com|net)|googletagmanager\.com).*/),
+  new workbox.strategies.NetworkOnly()
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*\/images.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "localImages-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*fontawesome.com.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "fontawesome-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*cdn.lordicon.com.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "lordicon-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*gstatic.com.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "gstatic.com-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*cdn.jsdelivr.net.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "cdn.jsdelivr.net-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*fonts.googleapis.com.*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "fonts.googleapis.com-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/.*(css|png|js|svg).*/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "misc-static-StaleWhileRevalidate"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/./),
+  new workbox.strategies.NetworkFirst({
+    cacheName: "default-NetworkFirst"
+  })
+);
+
+
